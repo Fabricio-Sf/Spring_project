1bc60a78d94184f50102c1a4f82202700bc06f97
Fabricio <fabricios4ntosfons3ca@gmail.com>
2026-01-31 15:30:25 -0300

    First commit

---
+++ CadastroSupremo/src/main/java/me/Flayvor/CadastroSupremo/CadastroSupremoApplication.java
@@ -0,0 +1,13 @@
+package me.Flayvor.CadastroSupremo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSupremoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSupremoApplication.class, args);
+	}
+
+}
---
+++ CadastroSupremo/src/test/java/me/Flayvor/CadastroSupremo/CadastroSupremoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Flayvor.CadastroSupremo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSupremoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
